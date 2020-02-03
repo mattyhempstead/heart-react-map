15058618236b1c863ee1b72912c21ea8cba76bd1
Matty <matty.hempstead@gmail.com>
2020-02-04 01:38:03 +1100

    Improve formatting and content of heart react map hover boxes

--- src/components/HeartReactMap/HeartReactMapElement/HeartReactMapHoverBox/HeartReactMapHoverBox.tsx
+++ src/components/HeartReactMap/HeartReactMapElement/HeartReactMapHoverBox/HeartReactMapHoverBox.tsx
@@ -10,17 +10,29 @@ class HeartReactMapHoverBox extends React.Component<Props> {
   render() {
     // Ensure box always renders inside of map
     const style = {
-      top: this.props.react.lat >= 15 ? '1vw' : '-16.5vw',
-      left: this.props.react.lon <= 0 ? '1vw' : '-11vw',
+      top: this.props.react.lat >= 15 ? '0.3vw' : '-16.5vw',
+      left: this.props.react.lon <= 0 ? '0.3vw' : '-11.0vw',
     }
 
     return (
       <div className="HeartReactMapHoverBox" style={style}>
-        {this.props.react.id}
+        <b>{this.props.react.id}</b>
         <br/>
-        {this.props.react.lat}
+        {this.props.react.timestamp.toString().slice(0, 24)}
         <br/>
-        {this.props.react.lon}
+        {this.props.react.lat}, {this.props.react.lon}
+        <br/><br/>
+        {this.props.react.city}
+        <br/>
+        {this.props.react.region}
+        <br/>
+        {this.props.react.country}
+        <br/>
+        {this.props.react.continent}
+        <br/><br/>
+        {this.props.react.uid}
+        <br/>
+        {this.props.react.utc_offset}
       </div>
     );  
   }
